4c0c6b1324054c7236a9ae44bfe643c90fb7ab15
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2023-11-23 04:42:55 +0530

    gg

---
+++ Components/quickaccess.php
@@ -0,0 +1,10 @@
+<div class='quickaccess_parent'>
+    <h3 id='h31'>Quick Access</h3>
+    <div class='quickaccess_container'> 
+        <a href='../Profile/profile.php' ><p class='qa_link'>Back</p></a>
+        <a href='../Postblog/postblog.php' ><p class='qa_link'>Post Blog</p></a>
+        <a href='../Myblogs/myblogs.php' ><p class='qa_link'>My Blogs</p></a>
+        <a href='../Postlisting/postlisting.php'><p class='qa_link'>Post Listing</p></a>
+        <a href='../Orders/orders.php'><p class='qa_link'>Orders</p></a>
+    </div>
+</div>
